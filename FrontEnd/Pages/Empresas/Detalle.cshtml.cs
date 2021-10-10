19e86f4504a2e4b2d650b34d60690355eb0a6449
Oscar Javier Gonzalez <oscar.gefallene@gmail.com>
2021-10-10 15:42:31 -0500

    Creacion Tablas de entidad Empresa

--- FrontEnd/Pages/Empresas/Detalle.cshtml.cs
+++ FrontEnd/Pages/Empresas/Detalle.cshtml.cs
@@ -13,10 +13,20 @@ namespace FrontEnd.Pages.Empresas
     public class DetalleModel : PageModel
     {
         private readonly IRepositorioEmpresa _repoEmpresa;
+        private readonly IRepositorioDirectivo _repoDirectivo;
+        private readonly IRepositorioEmpleado _repoEmpleado;
+        private readonly IRepositorioCliente _repoCliente;
+
+        public IEnumerable<Directivo> listadoDirectivos {get; set;}
+        public IEnumerable<Empleado> listadoEmpleados {get; set;}
+        public IEnumerable<Cliente> listadoClientes {get;set;}
         public Empresa Empresa {get; set;}
 
-        public DetalleModel(IRepositorioEmpresa repositorioEmpresa){
+        public DetalleModel(IRepositorioEmpresa repositorioEmpresa, IRepositorioDirectivo repositorioDirectivo, IRepositorioEmpleado repositorioEmpleado, IRepositorioCliente repositorioCliente){
             _repoEmpresa = repositorioEmpresa;
+            _repoDirectivo = repositorioDirectivo;
+            _repoEmpleado = repositorioEmpleado;
+            _repoCliente = repositorioCliente;
         }
         public IActionResult OnGet(int Id){
             Empresa = _repoEmpresa.ObtenerPorId(Id);
@@ -25,6 +35,10 @@ namespace FrontEnd.Pages.Empresas
             }else{
                 return Page();
             }
+            listadoDirectivos = _repoDirectivo.ObtenerPorIdEmpresa(Id);
+            listadoEmpleados = _repoEmpleado.ObtenerPorIdEmpresa(Id);
+            listadoClientes = _repoCliente.ObtenerPorIdEmpresa(Id);
+
         }
         
     }
