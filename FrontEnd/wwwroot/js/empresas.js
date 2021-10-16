7b18655f43a541b4a4d4fc47344e2b66aa372857
Diego Pineda <dfpr1998@hotmail.com>
2021-10-16 13:16:36 -0500

    Ajustes entidad Empresa

--- FrontEnd/wwwroot/js/empresas.js
+++ FrontEnd/wwwroot/js/empresas.js
@@ -1,100 +1,136 @@
 let estadoValorNombreEmpresa = false,
 	estadoValorNITEmpresa = false;
 
-function activarBotonCrear() {
+function activarBotonesCrearEditarEmpresa() {
+
 	if (estadoValorNombreEmpresa && estadoValorNITEmpresa) {
-		$('#CrearEmpresa').prop('disabled', false);
-		$('#ActualizarEmpresa').prop('disabled', false);
-	} else {
-		$('#CrearEmpresa').prop('disabled', true);
-		$('#ActualizarEmpresa').prop('disabled', true);
-	}
+
+        $("#CrearEmpresa").prop('disabled', false);
+        $("#ActualizarEmpresa").prop('disabled', false);
+    }
+    else {
+
+        $("#CrearEmpresa").prop('disabled', true);
+        $("#ActualizarEmpresa").prop('disabled', true);
+    }
 }
 
 $(document).ready(function() {
-	let idEmpresa = $('#IdEmpresa').val();
 
-	if (idEmpresa == undefined) {
-		estadoValorNombreEmpresa = false;
-		estadoValorNITEmpresa = false;
-	} else {
-		estadoValorNombreEmpresa = true;
-		estadoValorNITEmpresa = true;
-	}
+    let idEmpresa = $("#IdEmpresa").val();
+
+    if(idEmpresa == undefined) {
+
+        estadoValorNombreEmpresa = false;
+        estadoValorNITEmpresa = false;
+
+    }
+    else {
+
+        estadoValorNombreEmpresa = true;
+        estadoValorNITEmpresa = true;
+
+    }
+
+    console.log(idEmpresa)
+
 });
 
-$(document).ready(function() {
-	$('#NombreEmpresa').keyup(function() {
-		let valueInputNombreEmpresa = $('#NombreEmpresa').val();
+$(document).ready(function () {
 
-		if (/^([\s])*$/.test(valueInputNombreEmpresa)) {
-			$('#NombreEmpresa').removeClass('is-valid').addClass('is-invalid');
+    $("#NombreEmpresa").keyup(function () {
 
-			$('#MensajeValidacionCampoNombreEmpresa')
-				.html('Campo Nombre de Empresa Requerido.')
-				.removeClass('valid-feedback')
-				.addClass('invalid-feedback');
+        let valueInputNombreEmpresa = $("#NombreEmpresa").val();
+        console.log(valueInputNombreEmpresa);
 
-			estadoValorNombreEmpresa = false;
-			activarBotonCrear();
-		} else if (!/^([a-zA-ZñÑáÁéÉíÍóÓúÚ\s])*$/.test(valueInputNombreEmpresa)) {
-			$('#NombreEmpresa').removeClass('is-valid').addClass('is-invalid');
+        if ((/^([\s])*$/.test(valueInputNombreEmpresa))) {
 
-			$('#MensajeValidacionCampoNombreEmpresa')
-				.html('El valor ingresado no es válido.')
-				.removeClass('valid-feedback')
-				.addClass('invalid-feedback');
+            $("#NombreEmpresa").removeClass("is-valid")
+                .addClass("is-invalid");
+
+            $("#MensajeValidacionCampoNombreEmpresa").html("El campo Razon Social no puede estar vacío.")
+                .removeClass("valid-feedback")
+                .addClass("invalid-feedback");
 
 			estadoValorNombreEmpresa = false;
-			activarBotonCrear();
-		} else {
-			$('#NombreEmpresa').removeClass('is-invalid').addClass('is-valid');
 
-			$('#MensajeValidacionCampoNombreEmpresa')
-				.html('El valor ingresado es válido.')
-				.removeClass('invalid-feedback')
-				.addClass('valid-feedback');
+        }
+        else if (!(/^([a-zA-ZñÑáÁéÉíÍóÓúÚ\s.])*$/.test(valueInputNombreEmpresa))) {
+
+            $("#NombreEmpresa").removeClass("is-valid")
+                .addClass("is-invalid");
+
+            $("#MensajeValidacionCampoNombreEmpresa").html("El valor ingresado no es válido.")
+                .removeClass("valid-feedback")
+                .addClass("invalid-feedback");
+
+			estadoValorNombreEmpresa = false;
+
+        }
+        else {
+
+            $("#NombreEmpresa").removeClass("is-invalid")
+                .addClass("is-valid");
+
+            $("#MensajeValidacionCampoNombreEmpresa").html("El valor ingresado es válido.")
+                .removeClass("invalid-feedback")
+                .addClass("valid-feedback");
 
 			estadoValorNombreEmpresa = true;
-			activarBotonCrear();
-		}
-	});
+
+        }
+
+        activarBotonesCrearEditarEmpresa();
+
+    });
+
 });
 
-$(document).ready(function() {
-	$('#NITEmpresa').keyup(function() {
-		let valueInputNITEmpresa = $('#NITEmpresa').val();
+$(document).ready(function () {
 
-		if (/^([\s])*$/.test(valueInputNITEmpresa)) {
-			$('#NITEmpresa').removeClass('is-valid').addClass('is-invalid');
+    $("#NITEmpresa").keyup(function () {
 
-			$('#MensajeValidacionCampoNITEmpresa')
-				.html('El campo NIT de Empresa no puede estar vacío.')
-				.removeClass('valid-feedback')
-				.addClass('invalid-feedback');
+        let valueInputNITEmpresa = $("#NITEmpresa").val();
 
-			estadoValorNITEmpresa = false;
-			activarBotonCrear();
-		} else if (!/^([a-zA-ZñÑáÁéÉíÍóÓúÚ\s])*$/.test(valueInputNITEmpresa)) {
-			$('#NITEmpresa').removeClass('is-valid').addClass('is-invalid');
+        if ((/^([\s])*$/.test(valueInputNITEmpresa))) {
 
-			$('#MensajeValidacionCampoNITEmpresa')
-				.html('El valor ingresado no es válido.')
-				.removeClass('valid-feedback')
-				.addClass('invalid-feedback');
+            $("#NITEmpresa").removeClass("is-valid")
+                .addClass("is-invalid");
+
+            $("#MensajeValidacionCampoNITEmpresa").html("El campo NIT no puede estar vacío.")
+                .removeClass("valid-feedback")
+                .addClass("invalid-feedback");
 
 			estadoValorNITEmpresa = false;
-			activarBotonCrear();
-		} else {
-			$('#NITEmpresa').removeClass('is-invalid').addClass('is-valid');
 
-			$('#MensajeValidacionCampoNITEmpresa')
-				.html('El valor ingresado es válido.')
-				.removeClass('invalid-feedback')
-				.addClass('valid-feedback');
+        }
+        else if (!(/^([0-9])*$/.test(valueInputNITEmpresa))) {
+
+            $("#NITEmpresa").removeClass("is-valid")
+                .addClass("is-invalid");
+
+            $("#MensajeValidacionCampoNITEmpresa").html("El valor ingresado no es válido.")
+                .removeClass("valid-feedback")
+                .addClass("invalid-feedback");
+
+			estadoValorNITEmpresa = false;
+
+        }
+        else {
+
+            $("#NITEmpresa").removeClass("is-invalid")
+                .addClass("is-valid");
+
+            $("#MensajeValidacionCampoNITEmpresa").html("El valor ingresado es válido.")
+                .removeClass("invalid-feedback")
+                .addClass("valid-feedback");
 
 			estadoValorNITEmpresa = true;
-			activarBotonCrear();
-		}
-	});
+
+        }
+
+        activarBotonesCrearEditarEmpresa();
+
+    });
+
 });
